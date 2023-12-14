0428b628a75e8c011fc7ad98ac9f9056b65292e9
duncanram <duncan.ram@gmail.com>
2023-12-14 03:38:16 +0200

    errors fix

--- errors.c
+++ errors.c
@@ -1,16 +1,16 @@
 #include "shell.h"
 
 /**
- * _erratoi - converts strings to integers.
- * @s: The string to be converted
+ * _erratoi - Converts a string to an integer.
+ * @s: The string to be converted.
  *
- * Description: This function attempts to convert the provided string into an integer.
- * If no numbers are found in the string, it returns 0. If an error occurred
+ * This function attempts to convert the provided string into an integer.
+ * If no numbers are found in the string, it returns 0. If an error occurs
  * during conversion, it returns -1.
  *
  * Return: 0 if no numbers are found in the string,
- *         the converted number otherwise,
- *         or -1 on error (invalid input or overflow)
+ * the converted number otherwise,
+ *         or -1 on error.
  */
 int _erratoi(char *s)
 {
@@ -18,7 +18,7 @@ int _erratoi(char *s)
     unsigned long int result = 0;
 
     if (*s == '+')
-        s++;  /* TODO: why does this make main return 255? */
+        s++; /* TODO: why does this make main return 255? */
     for (i = 0; s[i] != '\0'; i++)
     {
         if (s[i] >= '0' && s[i] <= '9')
@@ -35,15 +35,15 @@ int _erratoi(char *s)
 }
 
 /**
- * print_error - displays error messages
- * @info: The parameter and return information struct
- * @estr: string containing the specified error type.
+ * print_error - Displays an error message.
+ * @info: The parameter and return information struct.
+ * @estr: String containing the specified error type.
  *
  * This function is responsible for printing out an error message based on
  * the provided error type string. It returns 0 in most cases but can return
  * -1 in case of an error.
  *
- * Return: 0 in normal cases, or -1 on error
+ * Return: 0 in normal cases, or -1 on error.
  */
 void print_error(info_t *info, char *estr)
 {
@@ -57,11 +57,11 @@ void print_error(info_t *info, char *estr)
 }
 
 /**
- * print_d - prints a decimal (integer) number in base 10.
- * @input: The input number
- * @fd: The file descriptor to write
+ * print_d - Prints a decimal (integer) number in base 10.
+ * @input: The input number.
+ * @fd: The file descriptor to write to.
  *
- * Return: The number of characters printed
+ * Return: The number of characters printed.
  */
 int print_d(int input, int fd)
 {
@@ -96,19 +96,20 @@ int print_d(int input, int fd)
 }
 
 /**
- * convert_number - conversion function similar to itoa
- * @num: The number to be converted
- * @base: the base for the conversion
- * @flags: flags related to the argument
+ * convert_number - Conversion function similar to itoa.
+ * @num: The number to be converted.
+ * @base: The base for the conversion.
+ * @flags: Flags related to the argument.
  *
  * This function converts the provided number into a string representation
  * using the specified base. It returns the resulting string.
  *
- * Return: the converted string
+ * Return: The converted string.
  */
 char *convert_number(long int num, int base, int flags)
 {
-    static char buffer[BUFFER_SIZE];
+    static char *array;
+    static char buffer[50];
     char sign = 0;
     char *ptr;
     unsigned long n = num;
@@ -118,12 +119,13 @@ char *convert_number(long int num, int base, int flags)
         n = -num;
         sign = '-';
     }
-    ptr = &buffer[BUFFER_SIZE - 1];
+    array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+    ptr = &buffer[49];
     *ptr = '\0';
 
     do
     {
-        *--ptr = (flags & CONVERT_LOWERCASE) ? "0123456789abcdef"[n % base] : "0123456789ABCDEF"[n % base];
+        *--ptr = array[n % base];
         n /= base;
     } while (n != 0);
 
@@ -133,13 +135,13 @@ char *convert_number(long int num, int base, int flags)
 }
 
 /**
- * remove_comments - replaces first instance of '#' in the string with '\0'
- * @buf: The address of the string to modify it.
+ * remove_comments - Replaces first instance of '#' in the string with '\0'
+ * @buf: The address of the string to modify.
  *
  * This function is designed to replace the first occurrence of '#' in the
- * given string with a null terminator ('\0'). It always returns 0
+ * given string with a null terminator ('\0'). It always returns 0.
  *
- * Return: always returns 0
+ * Return: Always returns 0.
  */
 void remove_comments(char *buf)
 {
